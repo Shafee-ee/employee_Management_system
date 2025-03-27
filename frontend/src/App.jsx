06adff6aef19c1c9ef5b7b6e6784df15930cabb2
shafee_azeem <shafeeghani@gmail.com>
2025-03-27 20:46:27 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,25 @@
 function App() {
 
   return (
-    <div>That fixed the issue</div>
+    <div>That fixed the issue
+      7 React Lessons I Wish I Knew Earlier
+      Every React Concept Explained
+      Explaining Pointers Until I Go Insane
+      The cloud is over-engineered and overpriced
+      Big Tech AI Is A Lie
+      What Your GitHub Account Says About You
+      Everything You'll Need to Know About Git with ThePrimeagen | Preview
+      40 APIs Every Developer Should Use (
+      What Your GitHub Account Says About You - *
+      How To Replace Video Games With Coding - **
+      Backend web development - a complete overview - *
+      The Only CSS Layout Guide You'll Ever Need
+      How to put an HTML website online (on the Internet)
+      God-Tier Developer Roadmap
+      Has Generative AI Already Peaked? - Computerphile
+      HOW TRANSISTORS RUN CODE?
+      Inside Task Manager with the
+    </div>
   )
 }
 
